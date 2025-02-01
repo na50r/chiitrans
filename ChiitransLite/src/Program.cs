b3bfb7acd0528b43b98fb55ee746fdf581ca153f
na50r <shanrosan@gmail.com>
2025-02-01 12:12:28 +0100

    change test platform target to x86

--- ChiitransLite/src/Program.cs
+++ ChiitransLite/src/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using ChiitransLite.forms;
 using ChiitransLite.translation.edict;
 using ChiitransLite.misc;
