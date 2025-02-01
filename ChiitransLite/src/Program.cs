eace3ee8fadf8e2a519ef94bf1a30ef5beef6779
na50r <shanrosan@gmail.com>
2025-02-01 12:07:12 +0100

    comment out/remove depreciated feat (remove error msg)

--- ChiitransLite/src/Program.cs
+++ ChiitransLite/src/Program.cs
@@ -5,14 +5,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using ChiitransLite.forms;
-using ChiitransLite.translation.atlas;
 using ChiitransLite.translation.edict;
 using ChiitransLite.misc;
 using System.Diagnostics;
 using System.Threading;
 
-namespace ChiitransLite {
-    static class Program {
+namespace ChiitransLite
+{
+    static class Program
+    {
 
         private static System.Threading.Timer settingsSaveTimer;
 
@@ -21,47 +22,54 @@ namespace ChiitransLite {
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] cmd) {
+        static void Main(string[] cmd)
+        {
             arguments = cmd;
             AppDomain.CurrentDomain.UnhandledException += (sender, args) => {
                 Exception ex = (Exception)args.ExceptionObject;
                 Logger.logException(ex);
             };
-            try {
+            try
+            {
                 Logger.log("Chiitrans Lite " + Application.ProductVersion.ToString());
-            } catch {
             }
-            Task.Factory.StartNew(() => {
-                Atlas.instance.initialize();
-            });
+            catch
+            {
+            }
             Task.Factory.StartNew(() => {
                 Edict.instance.initialize();
             });
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.ApplicationExit += new EventHandler((_1, _2) => {
-                try {
-                    if (settingsSaveTimer != null) {
+                try
+                {
+                    if (settingsSaveTimer != null)
+                    {
                         settingsSaveTimer.Change(Timeout.Infinite, Timeout.Infinite);
                     }
                     settings.Settings.app.save();
-                    Atlas.instance.close();
-                    texthook.TextHookInterop.TextHookCleanup();
-                } catch (Exception e) {
+                    //texthook.TextHookInterop.TextHookCleanup();
+                }
+                catch (Exception e)
+                {
                     Logger.logException(e);
                 }
-                
-                if (texthook.TextHook.instance.isCompat) {
+
+                if (texthook.TextHook.instance.isCompat)
+                {
                     Thread.Sleep(2500);
                     // goodbye, cruel ITH :(
                     Process.GetCurrentProcess().Kill();
                 }
             });
             settingsSaveTimer = new System.Threading.Timer(new TimerCallback((_) => {
-                try {
+                try
+                {
                     settings.Settings.app.save();
                     //Logger.log("Settings saved.");
-                } catch { }
+                }
+                catch { }
             }), null, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
             Application.Run(new MainForm());
         }
