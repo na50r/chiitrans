6d695d858eb8af9c587632a4854db4afec857bf4
na50r <shanrosan@gmail.com>
2025-02-01 12:14:14 +0100

    add a new unit test method

--- ChiitransLiteTests/UnitTest1.cs
+++ ChiitransLiteTests/UnitTest1.cs
@@ -2,12 +2,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ChiitransLite.misc;
 
-namespace ChiitransLiteTests {
+namespace ChiitransLiteTests
+{
     [TestClass]
-    public class UnitTest1 {
+    public class UnitTest1
+    {
         [TestMethod]
-        public void TestKataHira() {
+        public void TestKataHira1()
+        {
             string kata = "どー";
+            string hira = TextUtils.katakanaToHiragana(kata);
+            Assert.AreEqual("どう", hira);
+        }
+        [TestMethod]
+        public void TestKataHira2()
+        {
+            string kata = "ドー";
             string hira = TextUtils.katakanaToHiragana(kata);
             Assert.AreEqual("どう", hira);
         }
